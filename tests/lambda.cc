8fa9d1a4baf1a1cacc25b72599f6d646db73f60c
Hedzr Yeh <hedzrz@gmail.com>
2021-10-17 00:18:56 +0800

    added tests: partial-curry, type-name, lambda

--- tests/lambda.cc
+++ tests/lambda.cc
@@ -18,6 +18,8 @@
 #include <memory>
 #include <random>
 
+#include <mutex>
+
 namespace lambdas {
     void f(int n1, int n2, int n3, const int &n4, int n5) {
         std::cout << n1 << ' ' << n2 << ' ' << n3 << ' ' << n4 << ' ' << n5 << '\n';

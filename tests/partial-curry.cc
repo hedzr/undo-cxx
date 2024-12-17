4ff240853c057bf8b78b7aaefa0863ecf4462cd7
hz <hedzrz@gmail.com>
2024-12-18 05:12:29 +0800

    improved partial-curry.cc

--- tests/partial-curry.cc
+++ tests/partial-curry.cc
@@ -8,56 +8,45 @@
 // Created by Hedzr Yeh on 2021/10/13.
 //
 
-#include "undo_cxx.hh"
+#include "undo_cxx/undo-common.hh"
 
-#include <iomanip>
+#include <cmath>
 #include <iostream>
-#include <math.h>
-#include <string>
 
-#include <functional>
-#include <memory>
-#include <random>
+namespace {
 
-#include <deque>
-#include <list>
-#include <optional>
-#include <queue>
-#include <stack>
-#include <vector>
+  static int test(int x, int y, int z) {
+    return x + y + z;
+  }
 
-int test(int x, int y, int z) {
-  return x + y + z;
-}
+  static void test_partial() {
+    using namespace undo_cxx::util::cool;
+    std::cout << '\n';
 
-void test_partial() {
-  using namespace undo_cxx::util::cool;
-  std::cout << '\n';
+    auto f = partial(test, 5, 3);
+    std::cout << f(7) << '\n';
 
-  auto f = partial(test, 5, 3);
-  std::cout << f(7) << '\n';
+    std::cout << partial(test)(5, 3, 7) << '\n';
+    std::cout << partial(test, 5)(3, 7) << '\n';
+    std::cout << partial(test, 5, 3)(7) << '\n';
+  }
 
-  std::cout << partial(test)(5, 3, 7) << '\n';
-  std::cout << partial(test, 5)(3, 7) << '\n';
-  std::cout << partial(test, 5, 3)(7) << '\n';
-}
+  static void test_curry() {
+    using namespace undo_cxx::util::cool;
+    std::cout << '\n';
 
-void test_curry() {
-  using namespace undo_cxx::util::cool;
-  std::cout << '\n';
+    auto f = curry(test)(1);
+    auto g = f(2);
+    auto result = g(3);
+    auto result1 = curry(test)(1)(2)(3);
+    std::cout << "result:  " << result << '\n';
+    std::cout << "result1: " << result1 << '\n';
+  }
 
-  auto f = curry(test)(1);
-  auto g = f(2);
-  auto result = g(3);
-  auto result1 = curry(test)(1)(2)(3);
-  std::cout << "result:  " << result << '\n';
-  std::cout << "result1: " << result1 << '\n';
-}
+} // namespace
 
 int main() {
-
   test_partial();
   test_curry();
-
   return 0;
 }
